919b2c5ea13b483bfab5eeab14a742c0d3a89498
Sarthak Gupta <aries.sarthakgupta@gmail.com>
2026-08-03 04:25:17 +0530

    first commit

---
+++ Backend/src/app.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+
+const app = express()
+
+const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"]
+
+app.use(express.json())
+app.use(cookieParser())
+app.use((req, res, next) => {
+    const origin = req.headers.origin
+
+    if (origin && allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin)
+        res.setHeader("Access-Control-Allow-Credentials", "true")
+        res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization")
+        res.setHeader("Vary", "Origin")
+    }
+
+    if (req.method === "OPTIONS") {
+        return res.status(204).end()
+    }
+
+    next()
+})
+
+/* require all the routes here */
+const authRouter = require("./routes/auth.routes")
+const interviewRouter = require("./routes/interview.routes")
+
+
+/* using all the routes here */
+app.use("/api/auth", authRouter)
+app.use("/api/interview", interviewRouter)
+
+
+
+module.exports = app
