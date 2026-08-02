0c0be7080096f40625ebfc6750de5cee524cc443
Sarthak Gupta <aries.sarthakgupta@gmail.com>
2026-08-03 05:18:47 +0530

    deploy

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 
 const app = express()
 
-const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"]
+const allowedOrigins = ["https://interjd.vercel.app", "http://localhost:5173", "http://127.0.0.1:5173"]
 
 app.use(express.json())
 app.use(cookieParser())
